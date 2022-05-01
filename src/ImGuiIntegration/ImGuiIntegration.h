f6cf0ac8124f00b88e59efe0d56191e3f6a4ed31
stanleytang <tangshunlei@gmail.com>
2022-05-01 15:49:46 +0800

    Add util functions

--- src/ImGuiIntegration/ImGuiIntegration.h
+++ src/ImGuiIntegration/ImGuiIntegration.h
@@ -2,6 +2,8 @@
 
 #include "Common/CommonMacros.h"
 #include "Common/CommonTypes.h"
+#include "Eigen/Eigen"
+#include "Common/Math.h"
 
 #ifdef ImGuiIntegrationExport
 #define IMGUI_INTEGRATION_API __declspec(dllexport)
@@ -20,4 +22,13 @@ namespace ImGuiIntegration
 	IMGUI_INTEGRATION_API void			Shutdown();
 
 	IMGUI_INTEGRATION_API u64			WindowProcHandler(const u64 windowHandle, u32 msg, u64 wParam, u64 lParam);
+}
+
+namespace ImGui
+{
+	template <typename T>
+	ImVec2 FromVec2(const Vec2<T>& v) { return { f32(v.x()), f32(v.y()) }; }
+
+	template <typename T>
+	ImVec4 FromVec4(const Vec4<T>& v) { return { f32(v.x()), f32(v.y()), f32(v.z()), f32(v.w()) }; }
 }
