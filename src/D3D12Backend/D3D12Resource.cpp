c1ae3bc0b96ffe1ab839056749a1981f4c2d4598
stanleytang <tangshunlei@gmail.com>
2022-12-27 20:45:17 +0800

    Move class definition

--- src/D3D12Backend/D3D12Resource.cpp
+++ src/D3D12Backend/D3D12Resource.cpp
@@ -1,2 +1,146 @@
 #include "D3D12BackendPch.h"
 #include "D3D12Resource.h"
+#include "D3D12ResourceView.h"
+
+D3D12Backend::CommitedResource* D3D12Backend::CommitedResource::Builder::Build(D3D12Device* device, D3D12_HEAP_TYPE heapType)
+{
+	CommitedResource* result = new CommitedResource;
+
+	D3D12_RESOURCE_DESC desc = {};
+	{
+		desc.Dimension = mDimention;
+		desc.Alignment = mAlignment;
+		desc.Width = mWidth;
+		desc.Height = mHeight;
+		desc.DepthOrArraySize = mDepthOrArraySize;
+		desc.MipLevels = mMipLevels;
+		desc.Format = mFormat;
+		desc.SampleDesc = mSampleDesc;
+		desc.Layout = mLayout;
+		desc.Flags = mFlags;
+	}
+
+	// create gpu resource default as copy dest
+	ID3D12Resource* resource = nullptr;
+	CD3DX12_HEAP_PROPERTIES heapProp = CD3DX12_HEAP_PROPERTIES(heapType);
+	AssertHResultOk(device->GetDevice()->CreateCommittedResource(
+		&heapProp,
+		D3D12_HEAP_FLAG_NONE,
+		&desc,
+		mInitState,
+		nullptr,
+		IID_PPV_ARGS(&resource)));
+
+	NAME_RAW_D3D12_OBJECT(resource, mName);
+
+	result->mDevice = device;
+	result->mResource = resource;
+	result->mSize = { (i32)mWidth, (i32)mHeight, mDepthOrArraySize };
+	result->mDesc = desc;
+	result->mState = mInitState;
+
+	return result;
+}
+
+D3D12Backend::CommitedResource* D3D12Backend::CommitedResource::Possessor::Possess(D3D12Device* device)
+{
+	CommitedResource* result = new CommitedResource;
+
+	NAME_RAW_D3D12_OBJECT(mResource, mName);
+	const D3D12_RESOURCE_DESC& desc = mResource->GetDesc();
+
+	result->mDevice = device;
+	result->mResource = mResource;
+	result->mSize = { (i32)desc.Width, (i32)desc.Height, desc.DepthOrArraySize };
+	result->mDesc = desc;
+	result->mState = mCurrentState;
+
+	return result;
+}
+
+D3D12Backend::CommitedResource::~CommitedResource()
+{
+	mDevice->ReleaseD3D12Resource(mResource);
+}
+
+void D3D12Backend::CommitedResource::Transition(D3D12CommandContext* context, const D3D12_RESOURCE_STATES& destState)
+{
+	if (mState != destState)
+	{
+		context->Transition(mResource, mState, destState);
+		mState = destState;
+	}
+}
+
+D3D12Backend::CommitedResource::SrvBuilder D3D12Backend::CommitedResource::CreateSrv()
+{
+	return SrvBuilder().SetDevice(mDevice).SetResource(this);
+}
+
+D3D12Backend::CommitedResource::RtvBuilder D3D12Backend::CommitedResource::CreateRtv()
+{
+	return RtvBuilder().SetDevice(mDevice).SetResource(this);
+}
+
+D3D12Backend::CommitedResource::DsvBuilder D3D12Backend::CommitedResource::CreateDsv()
+{
+	return DsvBuilder().SetDevice(mDevice).SetResource(this);
+}
+
+SRV* D3D12Backend::CommitedResource::SrvBuilder::BuildTex2D()
+{
+	D3D12_SHADER_RESOURCE_VIEW_DESC desc = {};
+	{
+		desc.Format = mFormat;
+		desc.ViewDimension = mViewDimension;
+		desc.Shader4ComponentMapping = mShader4ComponentMapping;
+		desc.Texture2D.MostDetailedMip = mMostDetailedMip;
+		desc.Texture2D.MipLevels = mMipLevels;
+		desc.Texture2D.PlaneSlice = mPlaneSlice;
+		desc.Texture2D.ResourceMinLODClamp = mResourceMinLODClamp;
+	}
+
+	return new SRV(mDevice, mResource, desc);
+}
+
+RTV* D3D12Backend::CommitedResource::RtvBuilder::BuildTex2D()
+{
+	D3D12_RENDER_TARGET_VIEW_DESC desc = {};
+	{
+		desc.Format = mFormat;
+		desc.ViewDimension = mViewDimension;
+		desc.Texture2D.MipSlice = mMipSlice;
+		desc.Texture2D.PlaneSlice = mPlaneSlice;
+	}
+
+	return new RTV(mDevice, mResource, desc);
+}
+
+DSV* D3D12Backend::CommitedResource::DsvBuilder::BuildTex2D()
+{
+	D3D12_DEPTH_STENCIL_VIEW_DESC desc = {};
+	{
+		desc.Format = mFormat;
+		desc.ViewDimension = mViewDimension;
+		desc.Flags = mFlags;
+		desc.Texture2D.MipSlice = mMipSlice;
+	}
+
+	return new DSV(mDevice, mResource, desc);
+}
+
+D3D12Backend::CommitedResource* D3D12Backend::CreateCommitedResourceTex2D(D3D12Device* device, const Vec3i& size, DXGI_FORMAT format, D3D12_RESOURCE_FLAGS flags, D3D12_RESOURCE_STATES initStates, const char* name, u32 mipLevels)
+{
+	return D3D12Backend::CommitedResource::Builder()
+		.SetDimention(D3D12_RESOURCE_DIMENSION_TEXTURE2D)
+		.SetWidth(size.x())
+		.SetHeight(size.y())
+		.SetDepthOrArraySize(size.z())
+		.SetMipLevels(mipLevels)
+		.SetFormat(format)
+		.SetFlags(flags)
+		.SetName(name)
+		.SetInitState(initStates)
+		.BuildDefault(device);
+}
+
--- src/D3D12Backend/D3D12Resource.h
+++ src/D3D12Backend/D3D12Resource.h
@@ -3,6 +3,13 @@
 #include "D3D12Headers.h"
 #include "D3D12CommandContext.h"
 
+class SRV;
+class RTV;
+class UAV;
+class DSV;
+class D3D12Device;
+class D3D12CommandContext;
+
 class GD_D3D12BACKEND_API ID3D12Res
 {
 public:
@@ -12,3 +19,120 @@ public:
 	virtual std::string GetName() const { return {}; }
 };
 
+#define CAT2(X,Y) X##Y
+#define CAT(X,Y) CAT2(X,Y)
+
+#define SETTER(Type, Name) \
+	protected:	Type m##Name = {}; \
+	public:		using CAT(Temp, __LINE__) = Type; \
+				void Set##Name(const CAT(Temp, __LINE__) & Name) { m##Name = Name; }
+#define CONTINOUS_SETTER(Class, Type, Name) \
+	protected:	Type m##Name = {}; \
+	public:		using CAT(Temp, __LINE__) = Type; \
+				Class& Set##Name(const CAT(Temp, __LINE__) & Name) { m##Name = Name; return *this; }
+#define CONTINOUS_SETTER_VALUE(Class, Type, Name, DefValue)	\
+	protected:	Type m##Name = (DefValue); \
+	public:		using CAT(Temp, __LINE__) = Type; \
+				Class& Set##Name(const CAT(Temp, __LINE__) & Name) { m##Name = Name; return *this;  }
+
+namespace D3D12Backend
+{
+	class GD_D3D12BACKEND_API CommitedResource : public ID3D12Res
+	{
+	public:
+		class GD_D3D12BACKEND_API Builder
+		{
+			CONTINOUS_SETTER(Builder, D3D12_RESOURCE_DIMENSION, Dimention);
+			CONTINOUS_SETTER(Builder, u64, Alignment);
+			CONTINOUS_SETTER(Builder, u64, Width);
+			CONTINOUS_SETTER(Builder, u32, Height);
+			CONTINOUS_SETTER(Builder, u16, DepthOrArraySize);
+			CONTINOUS_SETTER(Builder, u16, MipLevels);
+			CONTINOUS_SETTER(Builder, DXGI_FORMAT, Format);
+			CONTINOUS_SETTER_VALUE(Builder, DXGI_SAMPLE_DESC, SampleDesc, (DXGI_SAMPLE_DESC{ 1, 0 }));
+			CONTINOUS_SETTER(Builder, D3D12_TEXTURE_LAYOUT, Layout);
+			CONTINOUS_SETTER(Builder, D3D12_RESOURCE_FLAGS, Flags);
+			CONTINOUS_SETTER(Builder, const char*, Name);
+			CONTINOUS_SETTER_VALUE(Builder, D3D12_RESOURCE_STATES, InitState, D3D12_RESOURCE_STATE_COMMON);
+
+		public:
+			CommitedResource* Build(D3D12Device* device, D3D12_HEAP_TYPE heapType);
+			CommitedResource* BuildUpload(D3D12Device* device) { return Build(device, D3D12_HEAP_TYPE_UPLOAD); }
+			CommitedResource* BuildDefault(D3D12Device* device) { return Build(device, D3D12_HEAP_TYPE_DEFAULT); }
+			CommitedResource* BuildReadback(D3D12Device* device) { return Build(device, D3D12_HEAP_TYPE_READBACK); }
+		};
+
+		class GD_D3D12BACKEND_API Possessor
+		{
+			CONTINOUS_SETTER(Possessor, ID3D12Resource*, Resource);
+			CONTINOUS_SETTER(Possessor, const char*, Name);
+			CONTINOUS_SETTER(Possessor, D3D12_RESOURCE_STATES, CurrentState);
+
+		public:
+			CommitedResource* Possess(D3D12Device* device);
+		};
+
+		class GD_D3D12BACKEND_API SrvBuilder
+		{
+			CONTINOUS_SETTER(SrvBuilder, D3D12Device*, Device);
+			CONTINOUS_SETTER(SrvBuilder, CommitedResource*, Resource);
+			CONTINOUS_SETTER(SrvBuilder, DXGI_FORMAT, Format);
+			CONTINOUS_SETTER(SrvBuilder, D3D12_SRV_DIMENSION, ViewDimension);
+			CONTINOUS_SETTER_VALUE(SrvBuilder, u32, Shader4ComponentMapping, D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING);
+			CONTINOUS_SETTER(SrvBuilder, u32, MostDetailedMip);
+			CONTINOUS_SETTER(SrvBuilder, u32, MipLevels);
+			CONTINOUS_SETTER(SrvBuilder, u32, PlaneSlice);
+			CONTINOUS_SETTER(SrvBuilder, f32, ResourceMinLODClamp);
+
+		public:
+			SRV* BuildTex2D();
+		};
+
+		class GD_D3D12BACKEND_API RtvBuilder
+		{
+			CONTINOUS_SETTER(RtvBuilder, D3D12Device*, Device);
+			CONTINOUS_SETTER(RtvBuilder, CommitedResource*, Resource);
+			CONTINOUS_SETTER(RtvBuilder, DXGI_FORMAT, Format);
+			CONTINOUS_SETTER(RtvBuilder, D3D12_RTV_DIMENSION, ViewDimension);
+			CONTINOUS_SETTER(RtvBuilder, u32, MipSlice);
+			CONTINOUS_SETTER(RtvBuilder, u32, PlaneSlice);
+
+		public:
+			RTV* BuildTex2D();
+		};
+
+		class GD_D3D12BACKEND_API DsvBuilder
+		{
+			CONTINOUS_SETTER(DsvBuilder, D3D12Device*, Device);
+			CONTINOUS_SETTER(DsvBuilder, CommitedResource*, Resource);
+			CONTINOUS_SETTER(DsvBuilder, DXGI_FORMAT, Format);
+			CONTINOUS_SETTER(DsvBuilder, D3D12_DSV_DIMENSION, ViewDimension);
+			CONTINOUS_SETTER(DsvBuilder, D3D12_DSV_FLAGS, Flags);
+			CONTINOUS_SETTER(DsvBuilder, u32, MipSlice);
+
+		public:
+			DSV* BuildTex2D();
+		};
+
+		~CommitedResource();
+		void						Transition(D3D12CommandContext* context, const D3D12_RESOURCE_STATES& destState) override;
+		ID3D12Resource* GetD3D12Resource() const override { return mResource; }
+		Vec3i						GetSize() const override { return mSize; }
+		DXGI_FORMAT					GetFormat() const { return mDesc.Format; } // TODO override?
+		u16							GetMipLevelCount() const { return mDesc.MipLevels; } // TODO override?
+		D3D12_RESOURCE_STATES		GetState() const { return mState; }
+
+		SrvBuilder					CreateSrv();
+		RtvBuilder					CreateRtv();
+		DsvBuilder					CreateDsv();
+
+	protected:
+		D3D12Device* mDevice = nullptr;
+		ID3D12Resource* mResource = nullptr;
+		Vec3i						mSize = {};
+		D3D12_RESOURCE_DESC			mDesc = {};
+		D3D12_RESOURCE_STATES		mState = D3D12_RESOURCE_STATE_COMMON;
+	};
+
+	GD_D3D12BACKEND_API CommitedResource* CreateCommitedResourceTex2D(D3D12Device* device, const Vec3i& size, DXGI_FORMAT format, D3D12_RESOURCE_FLAGS flags, D3D12_RESOURCE_STATES initStates, const char* name, u32 mipLevels = 1);
+}
