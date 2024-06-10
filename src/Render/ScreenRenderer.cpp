a8240adda110268d69314a1a344edfaafa21c6ff
stanleytang <tangshunlei@gmail.com>
2024-06-10 14:30:46 +0800

    Update screen renderer

--- src/Render/ScreenRenderer.cpp
+++ src/Render/ScreenRenderer.cpp
@@ -40,88 +40,153 @@ void ScreenRenderer::CalcSceneExposure(GI::IGraphicsInfra* infra, const GI::SrvU
 	const f32 brightMin = 4.f;
 	const f32 brightMax = 65536.f;
 	auto histogram = std::make_unique<RenderTarget>(infra, histogramSize, sizeof(u32), GI::Format::FORMAT_UNKNOWN, "BrightnessHistogram");
+	
+	auto frameGraph = mRenderModule->GetFrameGraph();
 
+	struct BrightnessHistogramPassData
 	{
-		RENDER_EVENT(infra, BrightnessHistogram);
+		GI::SrvUsage sceneHdr;
+		Vec3u sceneHdrSize;
+		GI::UavUsage histogram;
+		GI::RtvUsage target;
+	};
 
-		GI::ComputePass pass;
+	frameGraph->AddPass<BrightnessHistogramPassData>("BrightnessHistogram",
+		[&]
+		(RenderPassBuilder& builder, BrightnessHistogramPassData& data)
+		{
+			data.sceneHdr = builder.Read(sceneHdr);
+			data.sceneHdrSize = sceneHdr.GetResource()->GetSize();
+			data.histogram = builder.Write(histogram->GetUav());
+		},
+		[brightMin, brightMax, histogramSize]
+		(const BrightnessHistogramPassData& data, GI::IGraphicsInfra* infra)
+		{
+			RENDER_EVENT(infra, BrightnessHistogram);
 
-		pass.mRootSignatureDesc.mFile = "res/RootSignature/RootSignature.hlsl";
-		pass.mRootSignatureDesc.mEntry = "ComputeRS";
-		pass.mCsFile = "res/Shader/Exposure.hlsl";
-		pass.mShaderMacros.push_back(GI::ShaderMacro{ "CONSTRUCT_HISTOGRAM", "1" });
+			GI::ComputePass pass;
 
-		const Vec3u& size = sceneHdr.GetResource()->GetSize();
-		pass.AddSrv("SceneHdr", sceneHdr);
-		pass.AddCbVar("SceneHdrSize", Vec4f{ f32(size.x()), f32(size.y()), 1.f / size.x(), 1.f / size.y() });
+			pass.mRootSignatureDesc.mFile = "res/RootSignature/RootSignature.hlsl";
+			pass.mRootSignatureDesc.mEntry = "ComputeRS";
+			pass.mCsFile = "res/Shader/Exposure.hlsl";
+			pass.mShaderMacros.push_back(GI::ShaderMacro{ "CONSTRUCT_HISTOGRAM", "1" });
 
-		pass.AddUav("SceneBrightnessHistogram", histogram->GetUav());
-		pass.AddCbVar("HistogramInfo", Vec4f{ std::log2(brightMin), std::log2(brightMax), f32(histogramSize), 1.f / histogramSize });
+			const Vec3u& size = data.sceneHdrSize;
+			pass.AddSrv("SceneHdr", data.sceneHdr);
+			pass.AddCbVar("SceneHdrSize", Vec4f{ f32(size.x()), f32(size.y()), 1.f / size.x(), 1.f / size.y() });
 
-		pass.mThreadGroupCounts = { u32(size.x() / 32 + 1), u32(size.y() / 32 + 1), 1 };
+			pass.AddUav("SceneBrightnessHistogram", data.histogram);
+			pass.AddCbVar("HistogramInfo", Vec4f{ std::log2(brightMin), std::log2(brightMax), f32(histogramSize), 1.f / histogramSize });
 
-		infra->GetRecorder()->AddComputePass(pass);
-	}
+			pass.mThreadGroupCounts = { u32(size.x() / 32 + 1), u32(size.y() / 32 + 1), 1 };
 
+			infra->GetRecorder()->AddComputePass(pass);
+		});
+
+	struct HistogramReducePassData
 	{
-		RENDER_EVENT(infra, HistogramReduce);
+		Vec3u sceneHdrSize;
+		GI::SrvUsage histogram;
+		GI::UavUsage exposureRt;
+	};
 
-		GI::ComputePass pass;
+	frameGraph->AddPass<HistogramReducePassData>("HistogramReduce",
+		[&]
+		(RenderPassBuilder& builder, HistogramReducePassData& data)
+		{
+			data.sceneHdrSize = sceneHdr.GetResource()->GetSize();
+			data.histogram = builder.Read(histogram->GetSrv());
+			data.exposureRt = builder.Write(exposureRt);
+		},
+		[this, brightMin, brightMax, histogramSize]
+		(const HistogramReducePassData& data, GI::IGraphicsInfra* infra)
+		{
+			RENDER_EVENT(infra, HistogramReduce);
 
-		pass.mRootSignatureDesc.mFile = "res/RootSignature/RootSignature.hlsl";
-		pass.mRootSignatureDesc.mEntry = "ComputeRS";
-		pass.mCsFile = "res/Shader/Exposure.hlsl";
-		pass.mShaderMacros.push_back(GI::ShaderMacro{ "HISTOGRAM_REDUCE", "1" });
+			GI::ComputePass pass;
 
-		const Vec3u& size = sceneHdr.GetResource()->GetSize();
-		pass.AddCbVar("SceneHdrSize", Vec4f{ f32(size.x()), f32(size.y()), 1.f / size.x(), 1.f / size.y() });
+			pass.mRootSignatureDesc.mFile = "res/RootSignature/RootSignature.hlsl";
+			pass.mRootSignatureDesc.mEntry = "ComputeRS";
+			pass.mCsFile = "res/Shader/Exposure.hlsl";
+			pass.mShaderMacros.push_back(GI::ShaderMacro{ "HISTOGRAM_REDUCE", "1" });
 
-		pass.AddCbVar("TimeInfo", Vec4f{ mLastFrameDeltaTimeInSeconds, 1.f / mLastFrameDeltaTimeInSeconds, mSecondsSinceLaunch, 0.f });
+			const Vec3u& size = data.sceneHdrSize;
+			pass.AddCbVar("SceneHdrSize", Vec4f{ f32(size.x()), f32(size.y()), 1.f / size.x(), 1.f / size.y() });
 
-		pass.AddSrv("SceneBrightnessHistogram", histogram->GetSrv());
-		pass.AddCbVar("HistogramInfo", Vec4f{ std::log2(brightMin), std::log2(brightMax), f32(histogramSize), 1.f / histogramSize });
-		pass.AddCbVar("EyeAdaptInfo", Vec4f{ mEyeAdaptSpeedUp, mEyeAdaptSpeedDown, 0.f, 0.f });
-		
-		pass.AddUav("ExposureTexture", exposureRt);
+			pass.AddCbVar("TimeInfo", Vec4f{ mLastFrameDeltaTimeInSeconds, 1.f / mLastFrameDeltaTimeInSeconds, mSecondsSinceLaunch, 0.f });
 
-		pass.mThreadGroupCounts = { 1, 1, 1 };
+			pass.AddSrv("SceneBrightnessHistogram", data.histogram);
+			pass.AddCbVar("HistogramInfo", Vec4f{ std::log2(brightMin), std::log2(brightMax), f32(histogramSize), 1.f / histogramSize });
+			pass.AddCbVar("EyeAdaptInfo", Vec4f{ mEyeAdaptSpeedUp, mEyeAdaptSpeedDown, 0.f, 0.f });
 
-		infra->GetRecorder()->AddComputePass(pass);
-	}
+			pass.AddUav("ExposureTexture", data.exposureRt);
+
+			pass.mThreadGroupCounts = { 1, 1, 1 };
+
+			infra->GetRecorder()->AddComputePass(pass);
+		});
 }
 
 void ScreenRenderer::ToneMapping(GI::IGraphicsInfra* infra, const GI::SrvUsage& sceneHdr, const GI::SrvUsage& exposure, const GI::RtvUsage& target)
 {
-	RENDER_EVENT(infra, ToneMapping);
+	struct PassData
+	{
+		GI::VbvUsage geoVertices;
+		GI::IbvUsage geoIndices;
+		GI::SrvUsage sceneHdr;
+		GI::SrvUsage exposure;
+		GI::RtvUsage target;
+		Vec3u targetSize;
+	};
 
-	GI::GraphicsPass ldrScreenPass;
+	auto frameGraph = mRenderModule->GetFrameGraph();
+	frameGraph->AddPass<PassData>("GaussianBlur1D",
+		[&]
+		(RenderPassBuilder& builder, PassData& data)
+		{
+			data.geoVertices = builder.Read(mQuad->GetVbvDesc());
+			data.geoIndices = builder.Read(mQuad->GetIbvDesc());
+			data.sceneHdr = builder.Read(sceneHdr);
+			data.exposure = builder.Read(exposure);
+			data.targetSize = target.GetResource()->GetSize();
+			data.target = builder.Write(target);
+		},
+		[
+			inputLayout = mQuad->mVertexElementDescs,
+			indexCount = mQuad->mIndices.size()
+		]
+		(const PassData& data, GI::IGraphicsInfra* infra)
+		{
+			RENDER_EVENT(infra, ToneMapping);
 
-	ldrScreenPass.mRootSignatureDesc.mFile = "res/RootSignature/RootSignature.hlsl";
-	ldrScreenPass.mRootSignatureDesc.mEntry = "GraphicsRS";
-	ldrScreenPass.mVsFile = "res/Shader/ToneMapping.hlsl";
-	ldrScreenPass.mPsFile = "res/Shader/ToneMapping.hlsl";
+			GI::GraphicsPass ldrScreenPass;
 
-	ldrScreenPass.mDepthStencilDesc.SetDepthEnable(false);
-	ldrScreenPass.mDepthStencilDesc.SetStencilEnable(false);
+			ldrScreenPass.mRootSignatureDesc.mFile = "res/RootSignature/RootSignature.hlsl";
+			ldrScreenPass.mRootSignatureDesc.mEntry = "GraphicsRS";
+			ldrScreenPass.mVsFile = "res/Shader/ToneMapping.hlsl";
+			ldrScreenPass.mPsFile = "res/Shader/ToneMapping.hlsl";
 
-	ldrScreenPass.mInputLayout = mQuad->mVertexElementDescs;
+			ldrScreenPass.mDepthStencilDesc.SetDepthEnable(false);
+			ldrScreenPass.mDepthStencilDesc.SetStencilEnable(false);
 
-	const Vec3u& targetSize = target.GetResource()->GetSize();
-	
-	ldrScreenPass.AddCbVar("RtSize", Vec4f{ f32(targetSize.x()), f32(targetSize.y()), 1.f / targetSize.x(), 1.f / targetSize.y() });
+			ldrScreenPass.mInputLayout = inputLayout;
 
-	ldrScreenPass.AddSrv("SceneHdr", sceneHdr);
-	ldrScreenPass.AddSrv("ExposureTexture", exposure);
+			ldrScreenPass.AddCbVar("RtSize", Vec4f{ f32(data.targetSize.x()), f32(data.targetSize.y()), 1.f / data.targetSize.x(), 1.f / data.targetSize.y() });
 
-	ldrScreenPass.AddCbVar("ExposureInfo", Vec4f{ -4.f, 0.f, 0.f, 0.f });
+			// TODO missing sampler ???
+			ldrScreenPass.AddSrv("SceneHdr", data.sceneHdr);
+			ldrScreenPass.AddSrv("ExposureTexture", data.exposure);
 
-	ldrScreenPass.SetRtv(0, target);
-	ldrScreenPass.mViewPort.SetWidth(targetSize.x()).SetHeight(targetSize.y());
-	ldrScreenPass.mScissorRect = { 0, 0, i32(targetSize.x()), i32(targetSize.y()) };
-	
-	ldrScreenPass.PushVbv(mQuad->GetVbvDesc());
-	ldrScreenPass.SetIbv(mQuad->GetIbvDesc());
-	ldrScreenPass.mIndexCount = mQuad->mIndices.size();
+			ldrScreenPass.AddCbVar("ExposureInfo", Vec4f{ -4.f, 0.f, 0.f, 0.f });
 
-	infra->GetRecorder()->AddGraphicsPass(ldrScreenPass);
+			ldrScreenPass.SetRtv(0, data.target);
+			ldrScreenPass.mViewPort.SetWidth(data.targetSize.x()).SetHeight(data.targetSize.y());
+			ldrScreenPass.mScissorRect = { 0, 0, i32(data.targetSize.x()), i32(data.targetSize.y()) };
+
+			ldrScreenPass.PushVbv(data.geoVertices);
+			ldrScreenPass.SetIbv(data.geoIndices);
+			ldrScreenPass.mIndexCount = indexCount;
+
+			infra->GetRecorder()->AddGraphicsPass(ldrScreenPass);
+		});
 }
