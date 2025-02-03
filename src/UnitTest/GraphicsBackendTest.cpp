9388d7b6347b30abded26c952250b6d71d5633dd
stanleytang <tangshunlei@gmail.com>
2025-02-03 16:04:42 +0800

    Add hello texture

--- src/UnitTest/GraphicsBackendTest.cpp
+++ src/UnitTest/GraphicsBackendTest.cpp
@@ -161,5 +161,133 @@ namespace UnitTest
 			delete graphicsInfra;
 			msDestroyNativeWindow(window);
 		}
+
+		TEST_METHOD(HelloTexture_Succeed)
+		{
+			auto window = msCreateNativeWindow(L"TestWindow", Vec2u{ 640, 360 });
+			auto graphicsInfra = msCreateGraphicsInfra();
+
+			const auto windowInfo = window->GetInfo();
+			const auto& windowSize = windowInfo.mSize;
+
+			graphicsInfra->AdaptToWindow(windowInfo, 2);
+
+			auto texturePath = R"(res\Scene\lion.dds)";
+			auto image = graphicsInfra->CreateFromImageMemory(
+				Utils::GetTextureExtension(texturePath), 
+				Utils::LoadFileContent(texturePath), 
+				texturePath);
+
+			auto start = ch::high_resolution_clock::now();
+			while (ch::duration_cast<ch::seconds>(ch::high_resolution_clock::now() - start).count() < 3)
+			{
+				graphicsInfra->StartFrame();
+
+				auto texureResource = graphicsInfra->CreateMemoryResource(image->GetResourceDesc());
+				graphicsInfra->InitialMemoryResourceFromImage(texureResource.get(), *image);
+				const auto& imageDesc = image->GetResourceDesc();
+
+				GI::SrvUsage srv;
+				srv.mResource = texureResource.get();
+				srv.mDesc
+					.SetViewDimension(GI::SrvDimension::TEXTURE2D)
+					.SetFormat(GI::GetSrvFormat(imageDesc.GetFormat()))
+					.SetTexture2D_MipLevels(imageDesc.GetMipLevels());
+
+				std::vector<Vec2f> points =
+				{
+					Vec2f{ 0.f, 0.f },
+					Vec2f{ 1.f, 0.f },
+					Vec2f{ 1.f, 1.f },
+					Vec2f{ 0.f, 1.f },
+				};
+				std::vector<b8> vertices(points.size() * sizeof(Vec2f));
+				std::copy(points.begin(), points.end(), reinterpret_cast<Vec2f*>(vertices.data()));
+
+				auto vb = graphicsInfra->CreateMemoryResource(
+					GI::MemoryResourceDesc::Buffer2(vertices.size(), false, false, "Vertices")
+					.SetInitState(GI::ResourceState::STATE_GENERIC_READ)
+					.SetHeapType(GI::HeapType::UPLOAD));
+				graphicsInfra->CopyToUploadBufferResource(vb.get(), vertices);
+
+				GI::VbvUsage vbv;
+				vbv.mResource = vb.get();
+				vbv.mDesc
+					.SetSizeInBytes(vertices.size())
+					.SetStrideInBytes(sizeof(Vec2f));
+
+				GI::SamplerDesc sampler;
+				sampler
+					.SetAddressXYZ(GI::TextureAddressMode::WRAP)
+					.SetFilter(GI::Filter::MIN_MAG_MIP_LINEAR);
+
+				std::vector<u16> ids = { 0, 1, 2, 0, 2, 3 };
+				std::vector<b8> indices(ids.size() * sizeof(u16));
+				std::copy(ids.begin(), ids.end(), reinterpret_cast<u16*>(indices.data()));
+
+				auto ib = graphicsInfra->CreateMemoryResource(
+					GI::MemoryResourceDesc::Buffer2(indices.size(), false, false, "Indices")
+					.SetInitState(GI::ResourceState::STATE_GENERIC_READ)
+					.SetHeapType(GI::HeapType::UPLOAD));
+				graphicsInfra->CopyToUploadBufferResource(ib.get(), indices);
+
+				GI::IbvUsage ibv;
+				ibv.mResource = ib.get();
+				ibv.mDesc
+					.SetFormat(GI::Format::FORMAT_R16_UINT)
+					.SetSizeInBytes(indices.size());
+
+				std::vector<GI::InputElementDesc> inputDescs = {
+					GI::InputElementDesc()
+						.SetSemanticName("POSITION")
+						.SetSemanticIndex(0)
+						.SetFormat(GI::Format::FORMAT_R32G32_FLOAT)
+						.SetInputSlot(0)
+						.SetAlignedByteOffset(0)
+						.SetInputSlotClass(GI::InputClassification::PER_VERTEX_DATA)
+						.SetInstanceDataStepRate(0)
+				};
+
+				auto rt = graphicsInfra->GetWindowBackBuffer(windowInfo.mNativeHandle);
+
+				GI::RtvUsage rtv;
+				rtv.mResource = rt;
+				rtv.mDesc
+					.SetViewDimension(GI::RtvDimension::TEXTURE2D)
+					.SetFormat(GI::Format::FORMAT_R8G8B8A8_UNORM);
+
+				graphicsInfra->GetRecorder()->AddClearOperation(rtv, Vec4f{ 0.5f, 0.5f, 0.5f, 1.0f });
+
+				GI::GraphicsPass pass;
+				{
+					pass.SetShader("HelloTexture");
+
+					pass.SetupDepthStencil()
+						.SetDepthEnable(false)
+						.SetStencilEnable(false);
+
+					pass.SetRtv(0, rtv);
+					pass.mViewPort.SetTopLeftX(0).SetTopLeftY(0).SetWidth(windowSize.x()).SetHeight(windowSize.y());
+					pass.mScissorRect = { 0, 0, i32(windowSize.x()), i32(windowSize.y()) };
+
+					pass.SetGeometry(vbv, 0, inputDescs, ibv, 0, ids.size());
+
+					pass.AddCb4f("rtSize", Vec4f{ f32(windowSize.x()), f32(windowSize.y()), 1.f / windowSize.x(), 1.f / windowSize.y() });
+
+					pass.AddSrv("inputTex", srv);
+					pass.AddSampler("inputTexSampler", sampler);
+				}
+
+				graphicsInfra->GetRecorder()->AddGraphicsPass(pass);
+
+				graphicsInfra->EndFrame();
+
+				graphicsInfra->Present();
+			}
+
+			delete graphicsInfra;
+			msDestroyNativeWindow(window);
+		}
+
 	};
 }
