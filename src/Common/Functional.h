ddb3a80de8ddef8622a412897fc988fdecc67e63
stanleytang <tangshunlei@gmail.com>
2025-02-18 22:42:36 +0800

    Fix move only function

--- src/Common/Functional.h
+++ src/Common/Functional.h
@@ -4,28 +4,36 @@
 /* https://blog.rink.nu/2024/02/24/implementing-a-stdfunction-like-wrapper-in-c-part-1-type-erasing/
  */
 
-template <typename R, typename... Args>
+template <typename Ret, typename... Args>
 struct IMyFunc
 {
-	virtual R operator() (Args... args) = 0;
+	virtual Ret operator() (Args... args) = 0;
+
+	virtual void MoveTo(void* p) = 0;
 };
 
-template <typename F, typename R, typename... Args>
-struct MyFunc : IMyFunc<R, Args...>
+template <typename Callable, typename Ret, typename... Args>
+struct MyFunc : IMyFunc<Ret, Args...>
 {
-	F f;
-	MyFunc(F f) : f(std::move(f)) {}
-	R operator() (Args... args) override
+	Callable callable;
+	MyFunc(Callable c) : callable(std::move(c)) {}
+
+	Ret operator() (Args... args) override
 	{
-		return f(args...);
+		return callable(args...);
+	}
+
+	void MoveTo(void* p) override
+	{
+		auto c = new (p) MyFunc(std::move(callable));
 	}
 };
 
 template <typename T>
 struct MoveOnlyFunction;
 
-template <typename R, typename... Args>
-struct MoveOnlyFunction<R(Args...)>
+template <typename Ret, typename... Args>
+struct MoveOnlyFunction<Ret(Args...)>
 {
 	static inline constexpr size_t StorageSize = 64;
 	static inline constexpr size_t SmallObjectSize = StorageSize - sizeof(void*);
@@ -35,51 +43,84 @@ struct MoveOnlyFunction<R(Args...)>
 		std::max_align_t dummy1;
 		struct
 		{
-			std::array<std::byte, SmallObjectSize> buf;
-			IMyFunc<R, Args...>* ptr;
+			std::array<std::byte, SmallObjectSize> buffer;
+			IMyFunc<Ret, Args...>* ptr;
 		};
-	} s;
+	} storage;
 
-	template <typename F>
-	MoveOnlyFunction(F f)
+	template <typename Callable>
+	MoveOnlyFunction(Callable c)
 	{
-		if constexpr (sizeof(F) <= SmallObjectSize)
-		{
-			std::construct_at(
-				reinterpret_cast<MyFunc<F, R, Args...>*>(s.buf.data()),
-				std::move(f));
+		storage.buffer.fill({});
 
-			s.ptr = reinterpret_cast<MyFunc<F, R, Args...>*>(s.buf.data());
+		using T = MyFunc<Callable, Ret, Args...>;
+		if constexpr (sizeof(Callable) <= SmallObjectSize)
+		{
+			new (storage.buffer.data()) T(std::move(c));
+			storage.ptr = reinterpret_cast<T*>(storage.buffer.data());
 		}
 		else
 		{
-			s.ptr = new MyFunc<F, R, Args...>(std::move(f));
+			storage.ptr = new T(std::move(c));
 		}
+	}
+
+	MoveOnlyFunction()
+	{
+		storage.buffer.fill({});
+		storage.ptr = nullptr;
+	}
+	
+	MoveOnlyFunction(MoveOnlyFunction&& other)
+		: MoveOnlyFunction()
+	{
+		if (other.IsLocal())
+		{
+			other.storage.ptr->MoveTo(this->storage.buffer.data());
+			storage.ptr = reinterpret_cast<IMyFunc<Ret, Args...>*>(storage.buffer.data());
+		}
+		else
+		{
+			std::swap(storage.ptr, other.storage.ptr);
+		}
+	}
+
+	MoveOnlyFunction& operator=(MoveOnlyFunction&& other)
+	{
+		if (other.IsLocal())
+		{
+			other.storage.ptr->MoveTo(this->storage.buffer.data());
+		}
+		else
+		{
+			std::swap(storage.ptr, other.storage.ptr);
+		}
+		return *this;
 	}
 
 	~MoveOnlyFunction()
 	{
 		if (IsLocal())
 		{
-			std::destroy_at(s.ptr);
+			std::destroy_at(storage.ptr);
 		}
 		else
 		{
-			if (s.ptr != nullptr)
+			if (storage.ptr != nullptr)
 			{
-				delete s.ptr;
+				delete storage.ptr;
 			}
 		}
 	}
 
-	R operator() (Args... args)
+	Ret operator() (Args... args) const
 	{
-		return s.ptr->operator()(args...);
+		return storage.ptr->operator()(args...);
 	}
 
 private:
 	bool IsLocal() const noexcept
 	{
-		return s.ptr == reinterpret_cast<const IMyFunc<R, Args...>*>(s.buf.data());
+		return storage.ptr == reinterpret_cast<const IMyFunc<Ret, Args...>*>(storage.buffer.data());
 	}
 };
--- src/UnitTest/FunctionalTest.cpp
+++ src/UnitTest/FunctionalTest.cpp
@@ -10,18 +10,28 @@ namespace UnitTest
 	TEST_CLASS(FunctionalTest)
 	{
 	public:
-		TEST_METHOD(Test)
+		TEST_METHOD(CanCall)
 		{
 			auto p = std::make_unique<int>(1);
-
 			auto f = MoveOnlyFunction<int(int, int)>(
 				[p = std::move(p)](int a, int b)
 			{
 				return (*p) + a + b;
-			}
-			);
+			});
 
 			Assert::AreEqual(6, f(2, 3));
+		}
+
+		TEST_METHOD(CanMove)
+		{
+			auto p = std::make_unique<int>(1);
+			auto f = MoveOnlyFunction<int(int, int)>([p = std::move(p)](int a, int b)
+			{
+				return (*p) + a + b;
+			});
+			auto f1 = std::move(f);
+			auto r = f1(2, 3);
+			Assert::AreEqual(6, r);
 		}
 	};
 }
