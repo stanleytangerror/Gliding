81aa78cb97ece5bfb5b4a4857b793027ed949c54
stanleytang <tangshunlei@gmail.com>
2025-02-18 22:49:13 +0800

    Add test cases

--- src/UnitTest/FunctionalTest.cpp
+++ src/UnitTest/FunctionalTest.cpp
@@ -10,7 +10,7 @@ namespace UnitTest
 	TEST_CLASS(FunctionalTest)
 	{
 	public:
-		TEST_METHOD(CanCall)
+		TEST_METHOD(SmallLambda_CanCall)
 		{
 			auto p = std::make_unique<int>(1);
 			auto f = MoveOnlyFunction<int(int, int)>(
@@ -22,10 +22,38 @@ namespace UnitTest
 			Assert::AreEqual(6, f(2, 3));
 		}
 
-		TEST_METHOD(CanMove)
+		TEST_METHOD(SmallLambda_CanMove)
 		{
 			auto p = std::make_unique<int>(1);
-			auto f = MoveOnlyFunction<int(int, int)>([p = std::move(p)](int a, int b)
+			auto f = MoveOnlyFunction<int(int, int)>(
+				[p = std::move(p)](int a, int b)
+			{
+				return (*p) + a + b;
+			});
+			auto f1 = std::move(f);
+			auto r = f1(2, 3);
+			Assert::AreEqual(6, r);
+		}
+
+		TEST_METHOD(LargeLambda_CanCall)
+		{
+			auto p = std::make_unique<int>(1);
+			auto v = std::vector<std::string>(100, "test");
+			auto f = MoveOnlyFunction<int(int, int)>(
+				[p = std::move(p), v](int a, int b)
+			{
+				return (*p) + a + b;
+			});
+
+			Assert::AreEqual(6, f(2, 3));
+		}
+
+		TEST_METHOD(LargeLambda_CanMove)
+		{
+			auto p = std::make_unique<int>(1);
+			auto v = std::vector<std::string>(100, "test");
+			auto f = MoveOnlyFunction<int(int, int)>(
+				[p = std::move(p), v](int a, int b)
 			{
 				return (*p) + a + b;
 			});
